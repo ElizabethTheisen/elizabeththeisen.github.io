72fa6b838a6f6df5b356b129b896537ce05bbb9d
Elizabeth Theisen <etheisen@my.waketech.edu>
2025-05-05 22:05:45 -0400

    Added some information for Lesson 14

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,9 +1,29 @@
 // TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData (){
     //This function will query the PostgreSQL database and return grade data
-    console.log("Fetching grade data..")
+    console.log("Fetching grade data..");
+    // Create a new request for HTTP data
+    let xhr = new XMLHttpRequest();
+    // This is the address on the machine we're asking for data
+    let apiRoute = "/api/grades";
+    // When the request changes status, we run this anonymous function
+    xhr.onreadystatechange = function() {
+        // Check if we're done
+        if (xhr.readyState === xhr.DONE) {
+            // Check if we're successful
+            if (xhr.status !== 200) {
+                console.error(`Could not get grades. 
+                    Status: ${xhr.status}`);
+            
+        
+            }
+            // And then call the function to update the HTML with our data
+            populateGradebook(JSON.parse(xhr.responseText));
+        }
+    }.bind(this);
+    xhr.open("GET", apiRoute, true);
+    xhr.send();
 }
-
 // TODO: Populate the table with grade data
 function populateGradebook(data){
     //This function will take the fetched grade data and populate the table
